93eec20a5a4dd2d75fdeebeb06922a745591c392
Dan Leatherman <dleatherman@gmail.com>
2022-10-11 22:46:34 -0400

    base css and slider component

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,17 @@
+const CleanCSS = require("clean-css");
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
+
   eleventyConfig.addPlugin(pluginWebc, {
-    components: "./src/_includes/components/*.webc",
+    components: "./src/_includes/components/**/*.webc",
   });
+
+  // Minify CSS
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
   return {
     htmlTemplateEngine: "webc",
     dir: {
